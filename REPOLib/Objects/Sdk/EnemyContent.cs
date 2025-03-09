08f2fded5c33cf2a8a312f4826fb542634332cb6
Kesomannen <bobbo.nidor@gmail.com>
2025-03-09 08:31:08 +0100

    Add EnemyContent

---
+++ REPOLib/Objects/Sdk/EnemyContent.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace REPOLib.Objects.Sdk;
+
+[CreateAssetMenu(menuName = "REPOLib/Enemy", order = 2, fileName = "New Enemy")]
+public class EnemyContent : Content
+{
+    [SerializeField]
+    private EnemySetup _prefab;
+    
+    public EnemySetup Prefab => _prefab;
+
+    public override string Name => Prefab.name;
+
+    public override void Initialize(Mod mod)
+    {
+        Modules.Enemies.RegisterEnemy(Prefab);
+    }
+}
