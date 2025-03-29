1c36c3cd0f4bbdb85ef22b5f2214af73b225c07b
TheGuy920 <53882381+TheGuy920@users.noreply.github.com>
2025-03-29 02:25:01 -0400

    Update CommandManager.cs
    https://github.com/pardeike/Harmony/blob/master/Harmony/Tools/AccessTools.cs/#L83-L86

--- REPOLib/Commands/CommandManager.cs
+++ REPOLib/Commands/CommandManager.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
+using HarmonyLib;
 using REPOLib.Extensions;
 
 namespace REPOLib.Commands;
@@ -19,8 +20,7 @@ internal static class CommandManager
     {
         Logger.LogInfo($"CommandManager initializing.", extended: true);
 
-        CommandInitializerMethods = AppDomain.CurrentDomain.GetAssemblies()
-            .SelectMany(HarmonyLib.AccessTools.GetTypesFromAssembly)
+        CommandInitializerMethods = AccessTools.AllTypes()
             .SelectMany(type => type.SafeGetMethods())
             .Where(method => method?.GetCustomAttribute<CommandInitializerAttribute>() != null)
             .ToList();
@@ -59,8 +59,7 @@ internal static class CommandManager
 
     public static void FindAllCommandMethods()
     {
-        _commandExecutionMethodCache = AppDomain.CurrentDomain.GetAssemblies()
-            .SelectMany(HarmonyLib.AccessTools.GetTypesFromAssembly)
+        _commandExecutionMethodCache = AccessTools.AllTypes()
             .SelectMany(type => type.SafeGetMethods())
             .Where(method => method?.GetCustomAttribute<CommandExecutionAttribute>() != null)
             .ToList();
