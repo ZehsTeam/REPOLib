d09d977fba9d9d3e1fa3c2290955697c4698d54c
Kesomannen <bobbo.nidor@gmail.com>
2025-04-01 18:30:09 +0200

    Refactor ContentRegistry to use public generics

--- REPOLib/Modules/ContentRegistry.cs
+++ REPOLib/Modules/ContentRegistry.cs
@@ -39,17 +39,12 @@ public static class ContentRegistry
         }
     }
     
-    public static IContentSource? GetSource(string guid)
-    {
-        return _registry.Keys.FirstOrDefault(kvp => kvp.Guid == guid);
-    }
-    
     public static IEnumerable<(IContentSource Source, IReadOnlyList<Object> Content)> GetAll()
     {
-        return _registry.Select(kvp => (Source: kvp.Key, Content: (IReadOnlyList<Object>)kvp.Value));
+        return _registry.Select(kvp => (kvp.Key, (IReadOnlyList<Object>)kvp.Value));
     }
 
-    private static IEnumerable<(IContentSource, IEnumerable<T>)> GetAll<T>()
+    public static IEnumerable<(IContentSource Source, IEnumerable<T> Content)> GetAll<T>()
     {
         return _registry.Select(kvp => (kvp.Key, kvp.Value.OfType<T>()));
     }
@@ -59,29 +54,18 @@ public static class ContentRegistry
         return _registry.GetValueOrDefault(source);
     }
 
-    private static IEnumerable<T> GetAllFrom<T>(IContentSource source)
+    public static IEnumerable<T> GetAllFrom<T>(IContentSource source)
     {
         return GetAllFrom(source).OfType<T>();
     }
 
-    private static IContentSource? GetSourceInternal(Object obj)
+    public static IContentSource? GetSource(Object obj)
     {
         return _registry.FirstOrDefault(kvp => kvp.Value.Contains(obj)).Key;
     }
-
-    public static IEnumerable<(IContentSource Source, IEnumerable<ValuableObject> Valuables)> GetAllValuables() => GetAll<ValuableObject>();
-    public static IEnumerable<ValuableObject> GetAllValuablesFrom(IContentSource source) => GetAllFrom<ValuableObject>(source);
-    public static IContentSource? GetSource(ValuableObject valuable) => GetSourceInternal(valuable);
     
-    public static IEnumerable<(IContentSource Source, IEnumerable<Item> Items)> GetAllItems() => GetAll<Item>();
-    public static IEnumerable<Item> GetAllItemsFrom(IContentSource source) => GetAllFrom<Item>(source);
-    public static IContentSource? GetSource(Item item) => GetSourceInternal(item);
-    
-    public static IEnumerable<(IContentSource Source, IEnumerable<EnemySetup> Enemies)> GetAllEnemies() => GetAll<EnemySetup>();
-    public static IEnumerable<EnemySetup> GetAllEnemiesFrom(IContentSource source) => GetAllFrom<EnemySetup>(source);
-    public static IContentSource? GetSource(EnemySetup enemy) => GetSourceInternal(enemy);
-    
-    public static IEnumerable<(IContentSource Source, IEnumerable<Level> Levels)> GetAllLevels() => GetAll<Level>();
-    public static IEnumerable<Level> GetAllLevelsFrom(IContentSource source) => GetAllFrom<Level>(source);
-    public static IContentSource? GetSource(Level enemy) => GetSourceInternal(enemy);
+    public static IContentSource? GetSource(string guid)
+    {
+        return _registry.Keys.FirstOrDefault(kvp => kvp.Guid == guid);
+    }
 }
