7d58684be5ac442dcf223253c354340f198607be
TheGuy920 <53882381+TheGuy920@users.noreply.github.com>
2025-04-01 20:47:26 -0400

    Added attribute safety

--- REPOLib/Extensions/ReflectionExtensions.cs
+++ REPOLib/Extensions/ReflectionExtensions.cs
@@ -4,8 +4,17 @@ using System.Reflection;
 
 namespace REPOLib.Extensions;
 
+
+/// <summary>
+/// We add these extensions because we don't care about the reflection errors, and we can safely ignore them.
+/// </summary>
 internal static class ReflectionExtensions
 {
+    /// <summary>
+    /// A safe way to get all methods from a type.
+    /// </summary>
+    /// <param name="type"></param>
+    /// <returns></returns>
     public static IEnumerable<MethodInfo> SafeGetMethods(this Type type)
     {
         try
@@ -19,7 +28,13 @@ internal static class ReflectionExtensions
         }
     }
 
-    public static T? SafeGetCustomAttribute<T>(this MethodInfo method)
+    /// <summary>
+    /// A safe way to get a custom attribute from a method.
+    /// </summary>
+    /// <param name="method"></param>
+    /// <typeparam name="T"></typeparam>
+    /// <returns></returns>
+    public static T? SafeGetCustomAttribute<T>(this MethodInfo method) where T : Attribute
     {
         try
         {
@@ -30,5 +45,24 @@ internal static class ReflectionExtensions
             // Logger.LogWarning($"Error retrieving methods for type {type.FullName}: {ex.Message}");
             return null;
         }   
+    }
+    
+    /// <summary>
+    /// A safe way to check if a method has a custom attribute.
+    /// </summary>
+    /// <param name="method"></param>
+    /// <typeparam name="T"></typeparam>
+    /// <returns></returns>
+    public static bool HasCustomAttribute<T>(this MethodInfo method) where T : Attribute
+    {
+        try
+        {
+            return method.GetCustomAttribute<T>() != null;
+        }
+        catch /*(Exception ex)*/
+        {
+            // Logger.LogWarning($"Error retrieving methods for type {type.FullName}: {ex.Message}");
+            return false;
+        }
     }
 }
