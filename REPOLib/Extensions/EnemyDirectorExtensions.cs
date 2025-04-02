7c4e850c92a1bfe41c60785e6d6ee133d3ed3911
Hamunii <samuel.suni04@gmail.com>
2025-04-02 13:34:02 +0300

    document EnemyDirectorExtensions

--- REPOLib/Extensions/EnemyDirectorExtensions.cs
+++ REPOLib/Extensions/EnemyDirectorExtensions.cs
@@ -4,8 +4,17 @@ using System.Linq;
 
 namespace REPOLib.Extensions;
 
+/// <summary>
+/// REPOLib's <see cref="EnemyDirector"/> extension methods.
+/// </summary>
 public static class EnemyDirectorExtensions
 {
+    /// <summary>
+    /// Checks if the <see cref="EnemyDirector"/> contains the specified <see cref="EnemySetup"/>.
+    /// </summary>
+    /// <param name="enemyDirector">The <see cref="EnemyDirector"/> whose enemies to check.</param>
+    /// <param name="enemySetup">The <see cref="EnemySetup"/> to check against.</param>
+    /// <returns>Whether or not the <see cref="EnemyDirector"/> contains the specified <see cref="EnemySetup"/>.</returns>
     public static bool HasEnemy(this EnemyDirector enemyDirector, EnemySetup enemySetup)
     {
         if (enemySetup == null || enemySetup.spawnObjects.Count == 0)
@@ -58,10 +67,18 @@ public static class EnemyDirectorExtensions
             list.Add(enemySetup);
             return true;
         }
-        
+
         return false;
     }
-    
+
+    /// <summary>
+    /// Tries to get a list of <see cref="EnemySetup"/> with the specified <see cref="EnemyParent.Difficulty"/>
+    /// from the <see cref="EnemyDirector"/>.
+    /// </summary>
+    /// <param name="enemyDirector">The <see cref="EnemyDirector"/> whose enemies to get.</param>
+    /// <param name="difficultyType">The <see cref="EnemyParent.Difficulty"/> we want.</param>
+    /// <param name="list">The list of <see cref="EnemySetup"/> objects.</param>
+    /// <returns>Whether or not the the defined <see cref="EnemyParent.Difficulty"/> is in valid range and the corresponding enemy list is not null.</returns>
     public static bool TryGetList(this EnemyDirector enemyDirector, EnemyParent.Difficulty difficultyType, [NotNullWhen(true)] out List<EnemySetup>? list)
     {
         list = difficultyType switch
@@ -75,6 +92,11 @@ public static class EnemyDirectorExtensions
         return list != null;
     }
 
+    /// <summary>
+    /// Gets all <see cref="EnemySetup"/> objects.
+    /// </summary>
+    /// <param name="enemyDirector">The <see cref="EnemyDirector"/> whose enemies to get.</param>
+    /// <returns>A list of all <see cref="EnemySetup"/> objects.</returns>
     public static List<EnemySetup> GetEnemies(this EnemyDirector enemyDirector)
     {
         return [
@@ -84,24 +106,51 @@ public static class EnemyDirectorExtensions
         ];
     }
 
-    public static bool TryGetEnemyByName(this EnemyDirector enemyDirector, string name, out EnemySetup enemySetup)
+    /// <summary>
+    /// Tries to get an <see cref="EnemySetup"/> by name or by its <see cref="EnemyParent"/> name.
+    /// </summary>
+    /// <param name="enemyDirector">The <see cref="EnemyDirector"/> from which to check.</param>
+    /// <param name="name">The <see cref="string"/> to match.</param>
+    /// <param name="enemySetup">The found <see cref="EnemySetup"/>.</param>
+    /// <returns>Whether or not the <see cref="EnemySetup"/> was found.</returns>
+    public static bool TryGetEnemyByName(this EnemyDirector enemyDirector, string name, [NotNullWhen(true)] out EnemySetup? enemySetup)
     {
         enemySetup = enemyDirector.GetEnemyByName(name);
         return enemySetup != null;
     }
 
-    public static EnemySetup GetEnemyByName(this EnemyDirector enemyDirector, string name)
+    /// <summary>
+    /// Gets an <see cref="EnemySetup"/> by name or by its <see cref="EnemyParent"/> name.
+    /// </summary>
+    /// <param name="enemyDirector">The <see cref="EnemyDirector"/> from which to check.</param>
+    /// <param name="name">The <see cref="string"/> to match.</param>
+    /// <returns>The <see cref="EnemySetup"/> or null.</returns>
+    public static EnemySetup? GetEnemyByName(this EnemyDirector enemyDirector, string name)
     {
         return enemyDirector.GetEnemies()
             .FirstOrDefault(x => x.NameEquals(name));
     }
 
+
+  /// <summary>
+  /// Tries to get an <see cref="EnemySetup"/> that contains the name or by its <see cref="EnemyParent"/> that contains the name.
+  /// </summary>
+  /// <param name="enemyDirector">The <see cref="EnemyDirector"/> from which to check.</param>
+  /// <param name="name">The <see cref="string"/> to compare.</param>
+  /// <param name="enemySetup">The found <see cref="EnemySetup"/>.</param>
+  /// <returns>Whether or not the <see cref="EnemySetup"/> was found.</returns>
     public static bool TryGetEnemyThatContainsName(this EnemyDirector enemyDirector, string name, out EnemySetup enemySetup)
     {
         enemySetup = enemyDirector.GetEnemyThatContainsName(name);
         return enemySetup != null;
     }
 
+    /// <summary>
+    /// Gets an <see cref="EnemySetup"/> that contains the name or by its <see cref="EnemyParent"/> that contains the name.
+    /// </summary>
+    /// <param name="enemyDirector">The <see cref="EnemyDirector"/> from which to check.</param>
+    /// <param name="name">The <see cref="string"/> to compare.</param>
+    /// <returns>The <see cref="EnemySetup"/> or null.</returns>
     public static EnemySetup GetEnemyThatContainsName(this EnemyDirector enemyDirector, string name)
     {
         return enemyDirector.GetEnemies()
