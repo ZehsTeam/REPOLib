3bb30dc612f83d20d09623ee1d24c39936302673
Zehs <andrey5764zehsteam@gmail.com>
2025-04-28 13:19:49 -0500

    Updated Upgrades module.
    Added TryGetUpgrade method.
    Removed Upgrade method.
    Added AddLevel, RemoveLevel, and SetLevel methods.

--- REPOLib/Modules/Upgrades.cs
+++ REPOLib/Modules/Upgrades.cs
@@ -1,6 +1,7 @@
 using ExitGames.Client.Photon;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using UnityEngine;
 using Hashtable = ExitGames.Client.Photon.Hashtable;
@@ -76,11 +77,11 @@ public static class Upgrades
 
             try
             {
-                int value = upgrade.GetLevel(steamId);
+                int level = upgrade.GetLevel(steamId);
 
-                upgrade.StartAction.Invoke(playerAvatar, value);
+                upgrade.StartAction.Invoke(playerAvatar, level);
 
-                Logger.LogDebug($"PlayerUpgrade: Invoked start action for upgrade \"{upgrade.UpgradeId}\" on player \"{username}\" at level {value}");
+                Logger.LogDebug($"Upgrades: Invoked start action for upgrade \"{upgrade.UpgradeId}\" on player \"{username}\" at level {level}");
             }
             catch (Exception ex)
             {
@@ -89,7 +90,7 @@ public static class Upgrades
         }
     }
 
-    internal static void RaiseUpgradeEvent(string upgradeId, string steamId, int value)
+    internal static void RaiseUpgradeEvent(string upgradeId, string steamId, int level)
     {
         if (_upgradeEvent == null)
         {
@@ -100,7 +101,7 @@ public static class Upgrades
         {
             { "UpgradeId", upgradeId },
             { "SteamId", steamId },
-            { "Value", value }
+            { "Level", level }
         };
 
         _upgradeEvent.RaiseEvent(data, NetworkingEvents.RaiseOthers, SendOptions.SendReliable);
@@ -115,16 +116,12 @@ public static class Upgrades
 
         string upgradeId = (string)data["UpgradeId"];
         string steamId = (string)data["SteamId"];
-        int value = (int)data["Value"];
+        int level = (int)data["Level"];
 
-        PlayerUpgrade? playerUpgrade = GetUpgrade(upgradeId);
-
-        if (playerUpgrade == null)
+        if (TryGetUpgrade(upgradeId, out PlayerUpgrade? playerUpgrade))
         {
-            return;
+            playerUpgrade.ApplyUpgrade(steamId, level);
         }
-
-        playerUpgrade.ApplyUpgrade(steamId, value);
     }
 
     /// <summary>
@@ -134,6 +131,7 @@ public static class Upgrades
     /// <param name="item">The upgrade item if it has one.</param>
     /// <param name="startAction">The method to be called during <see cref="PlayerController.LateStart"/> to initialize values for the upgrade. (<see cref="PlayerAvatar"/> is the player being refrenced.) (<see cref="int"/> is the level of this upgrade that the player has.)</param>
     /// <param name="upgradeAction">The method called whenever an instance of this upgrade is triggered to change value. (<see cref="PlayerAvatar"/> is the player being refrenced.) (<see cref="int"/> is the level of this upgrade that the player has.)</param>
+    /// <returns>The newly registered <see cref="PlayerUpgrade"/> or null.</returns>
     public static PlayerUpgrade? RegisterUpgrade(string upgradeId, Item? item, Action<PlayerAvatar, int>? startAction, Action<PlayerAvatar, int>? upgradeAction)
     {
         if (_playerUpgrades.ContainsKey(upgradeId))
@@ -151,6 +149,7 @@ public static class Upgrades
     /// Retrieves a registered <see cref="PlayerUpgrade"/> by it's identifier.
     /// </summary>
     /// <param name="upgradeId">The ID of the upgrade.</param>
+    /// <returns>The <see cref="PlayerUpgrade"/> or null.</returns>
     public static PlayerUpgrade? GetUpgrade(string upgradeId)
     {
         if (_playerUpgrades.TryGetValue(upgradeId, out PlayerUpgrade playerUpgrade))
@@ -159,6 +158,18 @@ public static class Upgrades
         }
 
         return null;
+    }
+
+    /// <summary>
+    /// Tries to retrieves a registered <see cref="PlayerUpgrade"/> by it's identifier.
+    /// </summary>
+    /// <param name="upgradeId">The ID of the upgrade.</param>
+    /// <param name="playerUpgrade">The found <see cref="PlayerUpgrade"/>.</param>
+    /// <returns>Whether or not the <see cref="PlayerUpgrade"/> was found.</returns>
+    public static bool TryGetUpgrade(string upgradeId, [NotNullWhen(true)] out PlayerUpgrade? playerUpgrade)
+    {
+        playerUpgrade = GetUpgrade(upgradeId);
+        return playerUpgrade != null;
     }
 }
 
@@ -178,7 +189,7 @@ public class PlayerUpgrade
     public readonly Item? Item;
 
     /// <summary>
-    /// The dictionary of player identifiers and upgrade levels saved to the game file.
+    /// The dictionary of player Steam IDs and upgrade levels saved to the game file.
     /// </summary>
     public Dictionary<string, int> PlayerDictionary = [];
 
@@ -194,6 +205,7 @@ public class PlayerUpgrade
         _upgradeAction = upgradeAction;
     }
 
+    #region GetLevel
     /// <summary>
     /// Gets the level of this upgrade for the given player.
     /// </summary>
@@ -206,9 +218,9 @@ public class PlayerUpgrade
 
         return GetLevel(playerAvatar.steamID);
     }
-    
+
     /// <summary>
-    /// Gets the level of this upgrade for the given player's steamId.
+    /// Gets the level of this upgrade for the given player's Steam ID.
     /// </summary>
     public int GetLevel(string steamId)
     {
@@ -219,40 +231,114 @@ public class PlayerUpgrade
 
         return 0;
     }
+    #endregion
 
+    #region AddLevel
     /// <summary>
-    /// Triggers using this upgrade for the given player. (Calling this will network this change to other clients)
+    /// Adds the upgrade level(s) for the given player. (Calling this will network this change to other clients)
     /// </summary>
-    public int Upgrade(PlayerAvatar playerAvatar) 
+    /// <param name="playerAvatar">The player you want to add the upgrade level(s) to.</param>
+    /// <param name="amount">The amount of upgrade levels to add.</param>
+    /// <returns>The new upgrade level.</returns>
+    public int AddLevel(PlayerAvatar playerAvatar, int amount = 1)
     {
         if (playerAvatar == null)
         {
             return 0;
         }
 
-        return Upgrade(playerAvatar.steamID);
+        return AddLevel(playerAvatar.steamID, amount);
     }
-    
+
     /// <summary>
-    /// Triggers using this upgrade for the given player's steamId. (Calling this will network this change to other clients)
+    /// Adds the upgrade level(s) for the given player's Steam ID. (Calling this will network this change to other clients)
     /// </summary>
-	public int Upgrade(string steamId)
-	{
-        int value = GetLevel(steamId);
-        value++;
+    /// <param name="steamId">The player's Steam ID you want to add the upgrade level(s) to.</param>
+    /// <param name="amount">The amount of upgrade levels to add.</param>
+    /// <returns>The new upgrade level.</returns>
+    public int AddLevel(string steamId, int amount = 1)
+    {
+        return ChangeLevelBy(steamId, amount);
+    }
+    #endregion
 
-        PlayerDictionary[steamId] = value;
+    #region RemoveLevel
+    /// <summary>
+    /// Removes the upgrade level(s) for the given player. (Calling this will network this change to other clients)
+    /// </summary>
+    /// <param name="playerAvatar">The player you want to remove the upgrade level(s) from.</param>
+    /// <param name="amount">The amount of upgrade levels to remove.</param>
+    /// <returns>The new upgrade level.</returns>
+    public int RemoveLevel(PlayerAvatar playerAvatar, int amount = 1)
+    {
+        if (playerAvatar == null)
+        {
+            return 0;
+        }
 
-        ApplyUpgrade(steamId, value);
-
-        Upgrades.RaiseUpgradeEvent(UpgradeId, steamId, value);
-
-        return value;
+        return RemoveLevel(playerAvatar.steamID, amount);
     }
 
-    internal void ApplyUpgrade(string steamId, int value)
+    /// <summary>
+    /// Removes the upgrade level(s) for the given player's Steam ID. (Calling this will network this change to other clients)
+    /// </summary>
+    /// <param name="steamId">The player's Steam ID you want to remove the upgrade level(s) from.</param>
+    /// <param name="amount">The amount of upgrade levels to remove.</param>
+    /// <returns>The new upgrade level.</returns>
+    public int RemoveLevel(string steamId, int amount = 1)
     {
-        PlayerDictionary[steamId] = value;
+        return ChangeLevelBy(steamId, -amount);
+    }
+    #endregion
+
+    #region SetLevel
+    /// <summary>
+    /// Sets the upgrade level for the given player. (Calling this will network this change to other clients)
+    /// </summary>
+    /// <param name="playerAvatar">The player you want to set the upgrade level on.</param>
+    /// <param name="level">The new level you want to set.</param>
+    /// <returns>The new upgrade level.</returns>
+    public int SetLevel(PlayerAvatar playerAvatar, int level)
+    {
+        if (playerAvatar == null)
+        {
+            return 0;
+        }
+
+        return SetLevel(playerAvatar.steamID, level);
+    }
+
+    /// <summary>
+    /// Sets the upgrade level for the given player's Steam ID. (Calling this will network this change to other clients)
+    /// </summary>
+    /// <param name="steamId">The player's Steam ID you want to set the upgrade level on.</param>
+    /// <param name="level">The new level you want to set.</param>
+    /// <returns>The new upgrade level.</returns>
+    public int SetLevel(string steamId, int level)
+    {
+        level = Mathf.Max(level, 0);
+
+        PlayerDictionary[steamId] = level;
+
+        ApplyUpgrade(steamId, level);
+
+        Upgrades.RaiseUpgradeEvent(UpgradeId, steamId, level);
+
+        return level;
+    }
+    #endregion
+
+    private int ChangeLevelBy(string steamId, int amount)
+    {
+        int level = GetLevel(steamId);
+        level += amount;
+
+        return SetLevel(steamId, level);
+    }
+
+    internal void ApplyUpgrade(string steamId, int level)
+    {
+        PlayerDictionary[steamId] = level;
 
         if (_upgradeAction == null)
         {
@@ -272,7 +358,7 @@ public class PlayerUpgrade
         {
             _upgradeAction.Invoke(playerAvatar, GetLevel(steamId));
 
-            Logger.LogDebug($"PlayerUpgrade: Invoked upgrade action for upgrade \"{UpgradeId}\" on player \"{username}\" at level {value}");
+            Logger.LogDebug($"PlayerUpgrade: Invoked upgrade action for upgrade \"{UpgradeId}\" on player \"{username}\" at level {level}");
         }
         catch (Exception ex)
         {
@@ -328,14 +414,12 @@ public class REPOLibItemUpgrade : MonoBehaviour
             return;
         }
 
-        PlayerUpgrade? upgrade = Upgrades.GetUpgrade(UpgradeId);
-
-        if (upgrade == null)
+        if (!Upgrades.TryGetUpgrade(UpgradeId, out PlayerUpgrade? playerUpgrade))
         {
             Logger.LogError($"REPOLibItemUpgrade: Failed to upgrade \"{UpgradeId}\". Could not find PlayerUpgrade from UpgradeId.");
             return;
         }
 
-        upgrade.Upgrade(playerAvatar.steamID);
+        playerUpgrade.AddLevel(playerAvatar);
     }
 }
