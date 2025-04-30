7f20ba81d35ca6c96e7b08987d4bf7649b906624
TheGuy920 <53882381+TheGuy920@users.noreply.github.com>
2025-04-29 20:01:03 -0400

    wrapping

--- REPOLib/ConfigManager.cs
+++ REPOLib/ConfigManager.cs
@@ -13,33 +13,25 @@ internal static class ConfigManager
 
     public static ConfigFile ConfigFile
     {
-        get => _configFile ??
-               throw new NullReferenceException(
-                   $"{nameof(ConfigFile)} cannot be null. Did you forget to call {nameof(Initialize)}?");
+        get => _configFile ?? throw new NullReferenceException($"{nameof(ConfigFile)} cannot be null. Did you forget to call {nameof(Initialize)}?");
         private set => _configFile = value;
     }
 
     public static ConfigEntry<bool> ExtendedLogging
     {
-        get => _extendedLogging ??
-               throw new NullReferenceException(
-                   $"{nameof(ExtendedLogging)} cannot be null. Did you forget to call {nameof(Initialize)}?");
+        get => _extendedLogging ?? throw new NullReferenceException($"{nameof(ExtendedLogging)} cannot be null. Did you forget to call {nameof(Initialize)}?");
         private set => _extendedLogging = value;
     }
 
     public static ConfigEntry<bool> DeveloperMode
     {
-        get => _developerMode ??
-               throw new NullReferenceException(
-                   $"{nameof(DeveloperMode)} cannot be null. Did you forget to call {nameof(Initialize)}?");
+        get => _developerMode ?? throw new NullReferenceException($"{nameof(DeveloperMode)} cannot be null. Did you forget to call {nameof(Initialize)}?");
         private set => _developerMode = value;
     }
 
     public static ConfigEntry<bool> VanillaDeveloperMode
     {
-        get => _vanillaDeveloperMode ??
-               throw new NullReferenceException(
-                   $"{nameof(VanillaDeveloperMode)} cannot be null. Did you forget to call {nameof(Initialize)}?");
+        get => _vanillaDeveloperMode ?? throw new NullReferenceException($"{nameof(VanillaDeveloperMode)} cannot be null. Did you forget to call {nameof(Initialize)}?");
         private set => _vanillaDeveloperMode = value;
     }
 
@@ -49,8 +41,7 @@ internal static class ConfigManager
 
         ExtendedLogging = ConfigFile.Bind("General", "ExtendedLogging", false, "Enable extended logging.");
         DeveloperMode = ConfigFile.Bind("General", "DeveloperMode", false, "Enable developer mode cheats for testing.");
-        VanillaDeveloperMode = ConfigFile.Bind("General", "VanillaDeveloperMode", false,
-            "Enable vanilla developer mode cheats for testing.");
+        VanillaDeveloperMode = ConfigFile.Bind("General", "VanillaDeveloperMode", false, "Enable vanilla developer mode cheats for testing.");
         VanillaDeveloperMode.SettingChanged += (_, _) => SteamManagerPatch.UpdateDeveloperMode(SteamManager.instance);
     }
 }
